be93f1bb006e03cfed51f04a5dbc0edbdd3588e6
Fauzan Rifqy <fauzanriff@gmail.com>
2015-09-19 09:17:36 +0700

    0.1.1
    Updates
    - Input information from argv
    - Input handle when argv empty

--- receiver.cpp
+++ receiver.cpp
@@ -4,42 +4,56 @@
 //Problem : Aliran data dari transmitter ke receiver lebih cepat dibandingkan pemrosesan data di receiver.
 //Solution : Bentuk sebuah mekanisme aliran data dengan flow control, gunakan bahasa c++.
 
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
 //Receiver
 
-int main(){}
+int main(int argc, char *argv[]){
 //Init
-//Input port
+	string RSay= "Receiver :: ";
+	int port=8080;
+	//Input port
+	if (argv[1]){
+		port = atoi(argv[1]);
+		cout << RSay << "listening to : " << port << "\n";
+	}else{
+		cout << RSay << "listening to : 8080 (default)\n";
+	}
 //Terima Data
-//buat socket dan bind di port
-//buat proses anak
-	//Parent
-		//call rcvchar -> karakter dari socket simpan ke buffer until EOF
-			//buffer : circular buffer
-			//deteksi overflow : nCharInBuffer > minimum upperlimit
-				//YES overflow
-					//send XOFF
-												/*
-												*read the character into the buffer and increment the buffer pointers properly;
-												*if the number of characters in the buffer > minimum upperlimit then
-												*	send XOFF to the transmitter;
-												*/
-	//Child
-		//Terima data per-karakter q_get() -dari buffer-
-			//check karakter (ada&falid)
-			//deteksi emptySpace : nCharInBuffer < maximum lowerlimit
-				//YES emptySpace
-					//send XON
-												/*
-												*repeat
-									            *    if buffer count > 0 then
-												*		read a character from the buffer;
-												*		decrement the buffer count;
-												*		is it a valid character? ( >32, CR, LF atau end-of-file)
-												*until valid character;
-												*if buffer count < maximum lowerlimit then
-												*	send XON to the transmitter;
-												*/
-//Proses Data
+	//buat socket dan bind di port
+	//buat proses anak
+		//Parent
+			//call rcvchar -> karakter dari socket simpan ke buffer until EOF
+				//buffer : circular buffer
+				//deteksi overflow : nCharInBuffer > minimum upperlimit
+					//YES overflow
+						//send XOFF
+													/*
+													*read the character into the buffer and increment the buffer pointers properly;
+													*if the number of characters in the buffer > minimum upperlimit then
+													*	send XOFF to the transmitter;
+													*/
+		//Child
+			//Terima data per-karakter q_get() -dari buffer-
+				//check karakter (ada&falid)
+				//deteksi emptySpace : nCharInBuffer < maximum lowerlimit
+					//YES emptySpace
+						//send XON
+													/*
+													*repeat
+										            *    if buffer count > 0 then
+													*		read a character from the buffer;
+													*		decrement the buffer count;
+													*		is it a valid character? ( >32, CR, LF atau end-of-file)
+													*until valid character;
+													*if buffer count < maximum lowerlimit then
+													*	send XON to the transmitter;
+													*/
+				//Proses Data (buat delay)
+}
 
 /* Notes : 
 * - tambahkan delay antar rcvchar dan q_get() agar terjadi XOFF 
--- transmitter.cpp
+++ transmitter.cpp
@@ -4,11 +4,34 @@
 //Problem : Aliran data dari transmitter ke receiver lebih cepat dibandingkan pemrosesan data di receiver.
 //Solution : Bentuk sebuah mekanisme aliran data dengan flow control, gunakan bahasa c++.
 
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
 //Transmitter
 
 int main(){}
 //Init
-	//Input IP Adress, Hostname, Port Number
+	string TSay = "Transmitter :: ";
+	string host;
+	int port;
+	string filePath;
+	//Input IP Adress/Hostname, Port Number
+	if (argv[1]&&argv[2]&&argv[3]){
+		host = argv[1];
+		port = atoi(argv[2]);
+		filePath = argv[3];
+	}else{
+		cout << TSay << "Please input information below,\n";
+		cout << TSay << "IP Address/Hostname : ";
+		cin >> host;
+		cout << TSay << "Port : ";
+		cin >> port;
+		cout << TSay << "File Path : ";
+		cin >> filePath;
+	}
+	cout << TSay << "[" << host << "][" << port << "][" << filePath << "]\n";
 	//Input File
 //Proses Data
 	//Buat Socket
@@ -32,6 +55,6 @@ int main(){}
 															*	ch <- read a character from socket last received char <- ch
 															*	until connection is terminated
 															*/
-
+}
 
 /* Notes : Output harus jelas */
